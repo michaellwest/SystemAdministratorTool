4c1f306119250c7cac8466f7299dcfa5f3a8c620
Michael West <michaellwest@gmail.com>
2013-03-23 20:44:20 -0500

    Restructured files. Fixed AD query.

--- App.ps1
+++ App.ps1
@@ -1,4 +1,4 @@
-Import-Module ActiveDirectory
+Import-Module ActiveDirectory -PSSession (New-PSSession -ComputerName win2008r2dc)
 
 function New-GridView {
     param(
@@ -17,43 +17,49 @@ function New-GridView {
 
 Add-Type -AssemblyName "PresentationFramework"
 
-[xml]$xaml = (Get-Content -Path (Join-Path (Split-Path -Path $script:MyInvocation.MyCommand.Path) "MainWindow.xaml"))
+[xml]$xaml = Get-Content -Path (Join-Path -Path (Split-Path $script:MyInvocation.MyCommand.Path) -ChildPath  "MainWindow.xaml")
 $reader = New-Object System.Xml.XmlNodeReader $xaml 
-$window = [System.Windows.Markup.XamlReader]::Load($reader) 
+[System.Windows.Window]$window = [System.Windows.Markup.XamlReader]::Load($reader)
 
-$txtComputerName = [System.Windows.Controls.TextBox]($window.FindName('txtComputerName'))
+$window.FindName("MainFrame").Source = (Join-Path -Path (Split-Path $script:MyInvocation.MyCommand.Path) -ChildPath  "Admin.xaml")
+[xml]$xaml = Get-Content -Path (Join-Path -Path (Split-Path $script:MyInvocation.MyCommand.Path) -ChildPath  "Admin.xaml")
+$reader = New-Object System.Xml.XmlNodeReader $xaml 
+$window.Content = [System.Windows.Markup.XamlReader]::Load($reader)
+
+$content = $window.Content
+
+$txtComputerName = [System.Windows.Controls.TextBox]($content.FindName('txtComputerName'))
 $txtComputerName.Text = $Env:COMPUTERNAME
-$txtUserName = [System.Windows.Controls.TextBox]($window.FindName('txtUserName'))
+$txtUserName = [System.Windows.Controls.TextBox]($content.FindName('txtUserName'))
 $txtUserName.Text = $Env:USERNAME
 
-$listView = [System.Windows.Controls.ListView]($window.FindName('listView1'))
+$listView = [System.Windows.Controls.ListView]($content.FindName('listView1'))
 
-$btnProcesses = [System.Windows.Controls.Button]($window.FindName('btnProcesses'))
+$btnProcesses = [System.Windows.Controls.Button]($content.FindName('btnProcesses'))
 $btnProcesses.Add_Click({
     $processes = Get-Process -ComputerName $txtComputerName.Text
     $listView.View = New-GridView -Columns "Id","Name"
     $listView.ItemsSource = $processes
 })
 
-$btnServices = [System.Windows.Controls.Button]($window.FindName('btnServices'))
+$btnServices = [System.Windows.Controls.Button]($content.FindName('btnServices'))
 $btnServices.Add_Click({
     $services = Get-Service -ComputerName $txtComputerName.Text
     $listView.View = New-GridView -Columns "Status","Name","DisplayName"
     $listView.ItemsSource = $services
 })
 
-$btnSearch = [System.Windows.Controls.Button]($window.FindName('btnSearch'))
+$btnSearch = [System.Windows.Controls.Button]($content.FindName('btnSearch'))
 $btnSearch.Add_Click({
     $searchCriteria = $txtUserName.Text
     $props = @{
-        Filter = { Enabled -eq $true }
         Properties = "*"
         ResultSetSize = 100
     }
     if ($searchCriteria -like "*.*") {
-        $props["Filter"] = { Enabled -eq $true -and SamAccountName -like $searchCriteria }
+        $props["Filter"] = [scriptblock]::Create("Enabled -eq '$($true)' -and SamAccountName -like '$($searchCriteria)'")
     } else {
-        $props["Filter"] = { Enabled -eq $true -and (Surname -like $searchCriteria -or GivenName -like $searchCriteria -or samaccountname -like $searchCriteria) }
+        $props["Filter"] = [scriptblock]::Create("Enabled -eq '$($true)' -and (Surname -like '$($searchCriteria)' -or GivenName -like '$($searchCriteria)' -or samaccountname -like '$($searchCriteria)')")
     }
     $users = Get-ADUser @props | Sort-Object -Property Surname,GivenName
     $listView.View = New-GridView -Columns "DisplayName","GivenName","SurName","MiddleName","SamAccountName","Description","Office","Title","Manager","EmployeeID","EmailAddress","OfficePhone","HomeDirectory", "WhenChanged", "WhenCreated"
